f590974bef1f04a61fbaec90a4d73c6524b84145
roman <johndoe@email.com>
2025-12-04 19:56:24 +0300

    adjusted settings

---
+++ config.example.local.js
@@ -0,0 +1,14 @@
+// config.example.local.js
+module.exports = {
+    Servers: {
+        server1_tranq: {
+            client: "",
+            secret: ""
+        },
+        server1_sisi: {
+            client: "",
+            secret: ""
+        }
+    },
+    DiscordWHK: ""
+};
--- settings.js
+++ settings.js
@@ -1,32 +1,44 @@
+let localSecrets = {};
+
+try {
+	localSecrets = require("./config.local.js"); // локальные ключи
+} catch (e) {
+	console.error("‘айл config.local.js не найден! —оздайте его на основе config.example.local.js");
+	process.exit(1);
+}
+
+// ”ниверсальный путь к корневой папке сервера Ч работает везде
+const universalPath = path.join(__dirname);
+
 module.exports = {
 	"whook" : { 
-		"url" : "https://discord.com/api/webhooks/971463060801851463/Ut8bq1-2-vsoJrXWVIN544D_yBuTkxI_522vocgjVM6oBFY76VDL-NX0qG9lTDl2KXa2",
+		"url": localSecrets.DiscordWHK,
 		"name": 'Hole Controller',
 		"avatar": "https://imageserver.eveonline.com/Type/2062_64.png",
 		"welcomeMsg": "Hole Controller restarted."
 	},
 	"Servers":{
 		"server1_tranq" : {
-			"client" : "26463421869a4d34961b5f47a737da6b",
-			"secret" : "6nPDTexIIWMqRmMEk5gYMzPFWXvbrKgl8YDq4a7e",
+			"client": localSecrets.Servers.server1_tranq.client,
+			"secret": localSecrets.Servers.server1_tranq.secret,
 			"login" : "login.",
 			"port" : 3000,
 			"source" : "tranquility",
 			"file": "_tranq",
 			"token" : "stokens",
-			"path" : 'D:/Files/map/HoleController_Server',
+			"path": universalPath,
 			"corp": 605796230
 		},
-		"server1_sisi" : {
-			"client" : "26463421869a4d34961b5f47a737da6b",
-			"secret" : "6nPDTexIIWMqRmMEk5gYMzPFWXvbrKgl8YDq4a7e",
+		"server1_sisi": {
+			"client": localSecrets.Servers.server1_sisi.client,
+			"secret": localSecrets.Servers.server1_sisi.secret,
 			"login" : "sisilogin.test",
 			"port" : 8080,
 			// "source" : "singularity", //<-- CCP removed it 2020-01-13
 			"source" : "tranquility",
 			"file": "_sisi",
 			"token" : "ttokens",
-			"path" : 'D:/Files/map/HoleController_Server',
+			"path": universalPath,
 			"corp": 605796230
 		}
 	},
